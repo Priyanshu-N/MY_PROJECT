a93a1193f5d2cb515a883d8f7c4a139c56babbf9
priyanshu22cs25 <priyanshu96p@gmail.com>
2025-06-19 13:13:30 +0530

    adding code to git

---
+++ client/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile'
+import Header from './components/Header'
+// import Header from './components/Header'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return <BrowserRouter>
+  <Header />
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/About" element={<About/>} />
+    <Route path="/Signin" element={<Signin/>} />
+    <Route path="/Signup" element={<Signup/>} />
+    <Route path="/Profile" element={<Profile/>} />
+
+  </Routes>
+
+  </BrowserRouter>
+      
+  
+}
+
+export default App
